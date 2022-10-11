6ec9ecddcb4c449fb0af8a465d6edf2bee2a2161
AhmetfKasar <ahmetfkasar@gmail.com>
2022-10-11 15:05:33 +0300

    Staff permit relation

--- Context/Poco/StaffPermit.cs
+++ Context/Poco/StaffPermit.cs
@@ -6,11 +6,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace HekaMiniumApi.Context{
     public class StaffPermit{
         public int Id { get; set; }
+        [ForeignKey("Staff")]
         public int StaffId { get; set; }
         public string StaffPermitExplanation { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
         public int? PermitStatus { get; set; }
         public string StatusText { get; set; }
+
+        public virtual SysUser Staff { get; set;}
     }
 }
